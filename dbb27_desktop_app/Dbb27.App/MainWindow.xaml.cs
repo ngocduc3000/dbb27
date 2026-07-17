a34fa4695f4c3113a0a8d65a61651dd46b84c906
ngocduc3000 <nngocduc87@gmail.com>
2026-07-17 20:00:20 +0700

    feat: WPF-UI FluentWindow shell with dark theme

--- dbb27_desktop_app/Dbb27.App/MainWindow.xaml.cs
+++ dbb27_desktop_app/Dbb27.App/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -8,13 +7,14 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Wpf.Ui.Controls;
 
 namespace Dbb27.App;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
-public partial class MainWindow : Window
+public partial class MainWindow : FluentWindow
 {
     public MainWindow()
     {
